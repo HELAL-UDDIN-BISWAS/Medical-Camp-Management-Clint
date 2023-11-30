2e7d333a542fb1fcafce45dbb554eb980856254b
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2023-11-30 11:38:51 +0600

    added home page camp

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@
 
 import Ratings from '../Rating/Rating';
 import Banner from './Banner/Banner';
+import SixCamp from './Camps/SixCamp';
 import Swiperimg from './SwiperImg/Swiperimg';
 
 const Home = () => {
     return (
         <div className='max-h-screen'>
             <Banner></Banner>
+            <SixCamp></SixCamp>
             <Swiperimg></Swiperimg>
             <Ratings></Ratings>
         </div>
